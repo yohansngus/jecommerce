4a3f5ee279486614c98243b0394f2138d2fe9f04
yohansngus <yohansngus8@gmail.com>
2026-03-03 04:41:09 -0800

    I changed the grid cols from 4 to 2 in mobiel size

--- app/fashiongallery/page.tsx
+++ app/fashiongallery/page.tsx
@@ -8,7 +8,7 @@ export default function Fashiongallery() {
       <div className="bg-gray-100 my-2 mx-3 py-3 flex items-center justify-center font-mono text-2xl font-semibold text-shadow-[2px_2px_4px_rgba(0,0,0,0.2)]">
         Computer's
       </div>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2">
         <Card />
         <Card />
         <Card />
