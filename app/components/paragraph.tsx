050cea029d94d9114c7e7b6913e45832d31f5a82
yohansngus <yohansngus8@gmail.com>
2026-05-29 07:40:29 -0700

    update

--- app/components/paragraph.tsx
+++ app/components/paragraph.tsx
@@ -1,7 +1,7 @@
 export default function Paragraph() {
   return (
     <div className="mx-3 my-5 p-3 text-md font-sans text-gray-700 text-base leading-relaxed bg-gray-200 border-[0.5px] border-gray-300 shadow-sm lg:border-0 lg:shadow-none rounded-md">
-      <p className="text-lg font-sans leading-relaxed text-gray-600 p-3">
+      <p className="text-lg font-sans leading-relaxed text-gray-600 p-2">
         Lorem ipsum dolor amet consectetur, adipisicing . Minima quaerat dolore
         reiciendis quasi esse nrum blanditiis voluptas itaque qm suscipit?
         Commodi soluta quod quisquam quaerat sit ex id apiam hum! Lorm ipsum
@@ -9,7 +9,7 @@ export default function Paragraph() {
         reiciendis quasi esse nos dfd trum blanditiis voluptas itaque quam
         suscipit? Commodi soluta quod quisquam quaerat sfdf fdfex fjdkljk id apiam
         harum! Lorem ipsum dolor sit amet consectetur, bidflk fjdkf jflskdffdfss
-        df pisicing elit. Minima quaerat dolore reiciendis quasi essv gfgfe
+        df pisicing elit. Minima quaerat dolore reiciendis quasi essv gf fdf gfe
         ndsfd ostrum blanditiis voluptas itaque qgfm suscipit? ssf Commodi
         soluta quod quisquam qerat sit ex id harum! Lorem ipsum dolor sit amet
         consectetur, adipisicing elit. Minima quaerat dolore ywjf reiciendis
