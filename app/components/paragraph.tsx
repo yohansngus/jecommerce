e56166cc66506f9ff8507c071fbc206c87440f35
yohansngus <yohansngus8@gmail.com>
2026-06-01 10:09:51 -0700

    update

--- app/components/paragraph.tsx
+++ app/components/paragraph.tsx
@@ -1,27 +1,27 @@
 export default function Paragraph() {
   return (
     <div className="mx-3 my-5 p-3 text-md font-sans text-gray-700 text-base leading-relaxed bg-gray-200 border-[0.5px] border-gray-300 shadow-sm lg:border-0 lg:shadow-none rounded-md">
-      <p className="text-lg font-sans leading-relaxed text-gray-600 p-2">
+      <p className="text-lg font-sans leading-relaxed text-gray-600 p-3">
         Lorem ipsum dolor amet consectetur, adipisicing . Minima quaerat dolore
         reiciendis quasi esse nrum blanditiis voluptas itaque qm suscipit?
         Commodi soluta quod quisquam quaerat sit ex id apiam hum! Lorm ipsum
-        dolor sit amet consectetur, adipisicing elit. Min jhdjfhwk afd quaerat dolore
-        reiciendis quasi esse nos dfd trum blanditiis voluptas itaque quam
-        suscipit? Commodi soluta quod quisquam quaerat sfdf fdfex fjdkljk id apiam
-        harum! Lorem ipsum dolor sit amet consectetur, bidflk fjdkf jflskdffdfss
-        df pisicing elit. Minima quaerat dolore reiciendis quasi essv gf fdf gfe
-        ndsfd ostrum blanditiis voluptas itaque qgfm suscit? ssf Commodi
-        soluta quod quisquam qerat sit ex id harum! Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Minima quaerat dolore ywjf reiciendis
-        quasi esse nostrum blanditiis voluptas itaque quam suscipit? Commodi so
-        quod new quote quisquam aerat sit ex id afds dfdsf periam harum! Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Minima quaerat
-        dolore reiciendis quasi esse nostr blanditiis voluptas itaque quam
-        suscipit? Commodi soluta quod quisquam quae dsfsad fdsf rat sit ex id
-        aperiam harum! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Minima quaerat dolore reiciendis quasi esse nostrum blanditiis voluptas
-        itaque quam suscipit? Commodi soluta quod quisquam quaerat sit ex id
-        aperiam beru yand harum!
+        dolor sit amet consectetur, adipisicing elit. Min jhdjfhwk afd quaerat
+        dolore reiciendis quasi esse nos dfd trum blanditiis voluptas itaque
+        quam suscipit? Commodi soluta quod quisquam quaerat sfdf fdfex fjdkljk
+        id apiam harum! Lorem ipsum dolor sit amet consectetur, bidflk fjdkf
+        jflskdffdfss df pisicing elit. Minima quaerat dolore reiciendis quasi
+        essv gf fdf gfe ndsfd ostrum blanditiis voluptas itaque qgfm suscit? ssf
+        Commodi soluta quod quisquam qerat sit ex id harum! Lorem ipsum dolor
+        sit amet consectetur, adipisicing elit. Minima quaerat dolore jf
+        reiciendis quasi esse nostrum blanditiis voluptas itaque quam suscipit?
+        Commodi so quod new quote quisquam aerat sit ex id afds dfdsf periam
+        harum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+        quaerat dolore reiciendis quasi esse nostr blanditiis voluptas itaque
+        quam suscipit? Commodi soluta quod quisquam quae dsfsad fdsf rat sit ex
+        id aperiam harum! Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Minima quaerat dolore reiciendis quasi esse nostrum blanditiis
+        voluptas itaque quam suscipit? Commodi soluta quod quisquam quaerat sit
+        ex id aperiam beru yand harum!
       </p>
     </div>
   );
